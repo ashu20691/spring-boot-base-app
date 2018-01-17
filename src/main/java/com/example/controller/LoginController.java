6d5532f94856f278a2a92a3f9e7c10f00cad325b
Ashu Kumar <ashuashu20691@gmail.com>
2018-01-17 19:36:05 +0530

    added home page

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -20,14 +20,21 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public ModelAndView login(){
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("login");
 		return modelAndView;
 	}
-	
-	
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public ModelAndView dashboard(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home");
+		return modelAndView;
+	}
+
+
 //	@RequestMapping(value="/registration", method = RequestMethod.GET)
 //	public ModelAndView registration(){
 //		ModelAndView modelAndView = new ModelAndView();
